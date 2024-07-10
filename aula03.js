e78ad8ead148fd65bd7f4d1c878d9e03f532de8b
FABIO ROCHA <fabio_crono@hotmail.com>
2024-07-10 00:14:34 -0300

    primeiro comit

---
+++ aula03.js
@@ -0,0 +1,32 @@
+let n1 = 10 //number inteiro
+let n2 = 2 //numero inteiro
+let n3 = '2' //string texto
+
+document.write(`N1 ${n1} <br>`)
+document.write(`N2 ${n2} <br>`)
+document.write(`N3 ${n3} <br>`)
+
+console.log(n1 + n2) //Adição , soma
+console.log(n1 - n2) //subreação
+console.log(n1 * n2) //Multiplicação
+console.log(n1 / n2) //Divisão
+console.log(n1 % n2) //resto ou modulo
+
+console.log(n2 == n3) // igual a
+console.log(n2 === n3) //identico a
+console.log(n2 != n3) //difernete de Nao=
+console.log(n2!==n3) //não identico
+
+console.log(n1 > n2)
+console.log(n1 < n2)
+console.log(n1 >= n2)
+console.log(n1 <= n2)
+
+
+let a = true 
+let b = false
+console.log(a&&b) // E AND
+console.log(a||b) // OU OR
+console.log(!a) // Nao NOT negacao
+console.log(!b) //
+
